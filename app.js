04cb9fbd5d15a06d5b86fa180a91ee491e8cc93d
bukhuu <bekiybekiy60@gmail.com>
2022-10-19 18:01:43 +0800

    on

---
+++ app.js
@@ -0,0 +1,25 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч 
+
+var activePlayer = 1;
+
+// Тоглогчийн цуглуулсан хадгалах хувьсагч
+
+var scores = [0 , 0];
+
+
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+
+var roundScore = 0;
+
+
+// Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй , 1 6 гэсэн
+// утгыг энэ хувьсагчид санатсаргүйгээр үүсгэж өгнө 
+
+var dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+document.querySelector('.dice').style.displey = "none";
+console.log(dice);
