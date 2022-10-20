20f99add3d164f8cb3392677c05b9754dbdf92fa
bukhuu <bekiybekiy60@gmail.com>
2022-10-20 17:49:09 +0800

    update(2)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч 
 
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчийн цуглуулсан хадгалах хувьсагч
 
@@ -22,13 +22,49 @@ document.querySelector("#current-0").textContent = "0";
 document.querySelector("#current-1").textContent = "0";
 
 var diceDom = document.querySelector('.dice');
+    diceDom.style.displey = 'none';
+  
 
-// diceDom.style.displey = "none";
+ //шоог шидэх эвэнт 
 
 document.querySelector('.btn-roll').addEventListener("click", function(){
 
+    // 1 6 доторх санамсаргүй тоо гаргаж авах 
+
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    // шооны зураг вэб дээр гаргаж ирнэ
+
     diceDom.style.displey = "block";
+
+    // буусан санамсаргүй тоонд харгалзах шооны зургийг ваб дээр гаргаж ирнэ  
+
     diceDom.src = 'dice-'+diceNumber+'.png';
 
+    // тоглгчийн ээлжийн оноог өөрчилнө 
+
+    if(diceNumber !== 1){
+
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-'+activePlayer).textContent = roundScore;
+
+    }else{
+
+        roundScore = 0;
+
+        document.getElementById('current-'+activePlayer).textContent = 0;
+
+        if(activePlayer === 0){
+
+            activePlayer = 1; 
+
+        }else{
+
+            activePlayer = 0;
+        }
+
+        document.querySelector('.player-0-panel').classList.toggle("active");
+        document.querySelector('.player-1-panel').classList.toggle("active");
+        diceDom.style.displey = "none";
+    }
 });
