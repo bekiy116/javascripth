6f0058bd575e474853b1dc28fe9a6959edb12a89
bukhuu <bekiybekiy60@gmail.com>
2022-10-20 12:03:22 +0800

    update

--- app.js
+++ app.js
@@ -6,20 +6,29 @@ var activePlayer = 1;
 
 var scores = [0 , 0];
 
-
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 
 var roundScore = 0;
 
-
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй , 1 6 гэсэн
 // утгыг энэ хувьсагчид санатсаргүйгээр үүсгэж өгнө 
 
-var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector('.dice').style.displey = "none";
-console.log(dice);
+// программ эхлэхэд бэлтгэе 
+
+document.querySelector("#score-0").textContent = "0";
+document.querySelector("#score-1").textContent = "0";
+document.querySelector("#current-0").textContent = "0";
+document.querySelector("#current-1").textContent = "0";
+
+var diceDom = document.querySelector('.dice');
+
+// diceDom.style.displey = "none";
+
+document.querySelector('.btn-roll').addEventListener("click", function(){
+
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.displey = "block";
+    diceDom.src = 'dice-'+diceNumber+'.png';
+
+});
